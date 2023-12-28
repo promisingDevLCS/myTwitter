c2d6320f8b4853f0501093bed7c8d0c5da78a55a
promisingDevLCS <changseog8701@gmail.com>
2023-12-28 23:24:55 +0900

    Home, Login, Profile 등 여러 페이지에 대한 라우팅 설정

--- MyTwitter/src/App.tsx
+++ MyTwitter/src/App.tsx
@@ -1,7 +1,42 @@
 import { useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/createAccount";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/createAccount",
+    element: <CreateAccount />,
+  },
+]);
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ MyTwitter/src/routes/createAccount.tsx
@@ -0,0 +1,3 @@
+export default function createAccount() {
+  return <h1>createAccount</h1>;
+}
