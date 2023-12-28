73be35ba1c960b97185c6c2c5cbdb0fff6681e92
promisingDevLCS <changseog8701@gmail.com>
2023-12-28 23:51:03 +0900

    firebase 초기 설정

---
+++ MyTwitter/src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA91xujYYdcT6qQolh-lJywEUfzUV77fbA",
+  authDomain: "mytwitter-9a0a0.firebaseapp.com",
+  projectId: "mytwitter-9a0a0",
+  storageBucket: "mytwitter-9a0a0.appspot.com",
+  messagingSenderId: "273078736728",
+  appId: "1:273078736728:web:d05de000ee10c6a8ca772d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
